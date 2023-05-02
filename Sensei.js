d90ca758d387fc098c84fbd4720abb52d007d9f2
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-05-02 09:50:25 -0700

    Finished Assignment

--- Sensei.js
+++ Sensei.js
@@ -20,9 +20,31 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    super(name){
+        this.name = name
+        this.health = 200
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+    speakWisdom(){
+        this.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+
+    // speakWisdom method should call the drink sake method from ninja class before console.logging a wise message
+}
 
 const ninja1 = new Ninja("Hyabusa")
 ninja1.sayName()
 ninja1.showStats()
 ninja1.drinkSake()
 ninja1.showStats()
+
+const sensei1 = new Sensei("Master Splinter");
+console.log(sensei1);
+sensei1.speakWisdom();
+sensei1.showStats();
+
+ninja1.showStats
