0573aff38d68d1d1a300d67451a2fc763905b9b3
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-05-02 08:50:37 -0700

    Project Completed

---
+++ Ninja.js
@@ -0,0 +1,28 @@
+class Ninja {
+    constructor (name, health, speed, strength){
+        this.name = name
+        this.health = 3
+        this.speed = 3
+        this.strength = 3
+    }
+    sayName() {
+        console.log("Hello my name is "  + this.name)
+    }
+
+    showStats() {
+        console.log("Name: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Speed: " + this.speed);
+        console.log("Strength: " + this.strength);
+    }
+    drinkSake() {
+        this.health += 10
+    }
+}
+
+
+const ninja1 = new Ninja("Hyabusa")
+ninja1.sayName()
+ninja1.showStats()
+ninja1.drinkSake()
+ninja1.showStats()
